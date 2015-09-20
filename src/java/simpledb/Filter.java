b4a7e24dd994446555e80a9538edcc66f75c7f9e
JustinSty <jiushiwosty@berkeley.edu>
2015-09-20 14:42:11 -0700

    filter

--- src/java/simpledb/Filter.java
+++ src/java/simpledb/Filter.java
@@ -10,7 +10,8 @@ public class Filter extends Operator {
     private static final long serialVersionUID = 1L;
     private Predicate pred;
     private DbIterator child;
-
+    
+    /* check DbIterator and Predicate class
     /**
      * Constructor accepts a predicate to apply and a child operator to read
      * tuples to filter from.
@@ -22,11 +23,13 @@ public class Filter extends Operator {
      */
     public Filter(Predicate p, DbIterator child) {
         // IMPLEMENT ME
+        this.pred = p;
+        this.child = child;
     }
 
     public Predicate getPredicate() {
         // IMPLEMENT ME
-        return null;
+        return pred;
     }
 
     /**
@@ -34,20 +37,25 @@ public class Filter extends Operator {
      */
     public TupleDesc getTupleDesc() {
         // IMPLEMENT ME
-        return null;
+        return child.getTupleDesc();
     }
 
     public void open() throws DbException, NoSuchElementException,
             TransactionAbortedException {
         // IMPLEMENT ME
+        child.open();
+        super.open();
     }
 
     public void close() {
         // IMPLEMENT ME
+        super.close();
+        child.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // IMPLEMENT ME
+        child.rewind();
     }
 
     /**
@@ -62,6 +70,12 @@ public class Filter extends Operator {
     protected Tuple fetchNext() throws NoSuchElementException,
             TransactionAbortedException, DbException {
         // IMPLEMENT ME
+        while (child.hasNext()){
+            Tuple tp = child.next();
+            if (pred.filter(tp)) {  /*boolean*/
+                return tp;
+            }
+        }
         return null;
     }
 
